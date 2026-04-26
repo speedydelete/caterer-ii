090544f05f098425873813913be09eee9b47eadb
speedydelete <speedykorok777@gmail.com>
2026-04-26 14:08:52 -0400

    pls work...

--- src/worker.ts
+++ src/worker.ts
@@ -577,7 +577,8 @@ async function runSim(argv: string[], rle: string): Promise<[number, string | un
         let endY = startY + pHeight;
         let pData = p.getData();
         let index = 0;
-        gifData.push(new Uint8Array([0x21, 0xf9, 0x04, 0x00, time & 255, (time >> 8) & 255, 0xff, 0x00, 0x2c, 0x00, 0x00, 0x00, 0x00, width & 255, (width >> 8) & 255, height & 255, (height >> 8) & 255, 0x00]));
+        gifData.push(new Uint8Array([0x21, 0xf9, 0x04, 0x00, time & 255, (time >> 8) & 255, 0xff, 0x00, 0x2c, 0x00, 0x00, 0x00, 0x00, width & 255, (width >> 8) & 255, height & 255, (height >> 8) & 255, 0x00, bitWidth]));
+        let datas: number[][] = [];
         let data: number[] = [];
         for (let y = 0; y < startY; y++) {
             for (let x = 0; x < width; x++) {
@@ -638,6 +639,10 @@ async function runSim(argv: string[], rle: string): Promise<[number, string | un
                 } else {
                     data.push(value);
                 }
+                if (data.length > (1 << 24)) {
+                    datas.push(data);
+                    data = [];
+                }
             }
             for (let x = endX; x < width; x++) {
                 data.push(clearCode, 0);
@@ -649,27 +654,36 @@ async function runSim(argv: string[], rle: string): Promise<[number, string | un
             }
         }
         data.push(endCode);
+        datas.push(data);
+        let outs: Uint8Array[] = [];
         let out: number[] = [];
         let accumulator = 0;
         let bitCount = 0;
-        for (let value of data) {
-            accumulator |= value << bitCount;
-            bitCount += codeSize;
-            while (bitCount >= 8) {
-                out.push(accumulator & 0xff);
-                accumulator >>= 8;
-                bitCount -= 8;
+        for (let data of datas) {
+            for (let value of data) {
+                accumulator |= value << bitCount;
+                bitCount += codeSize;
+                while (bitCount >= 8) {
+                    out.push(accumulator & 0xff);
+                    accumulator >>= 8;
+                    bitCount -= 8;
+                    if (out.length > (1 << 24)) {
+                        outs.push(new Uint8Array(out));
+                    }
+                }
             }
         }
         if (bitCount > 0) {
             out.push(accumulator & 0xff);
         }
-        gifData.push(new Uint8Array([bitWidth]));
-        let i = 0;
-        while (i < out.length) {
-            let length = Math.min(255, out.length - i);
-            gifData.push(new Uint8Array([length, ...out.slice(i, i + length)]));
-            i += length;
+        outs.push(new Uint8Array(out));
+        for (let out of outs) {
+            let i = 0;
+            while (i < out.length) {
+                let length = Math.min(255, out.length - i);
+                gifData.push(new Uint8Array([length, ...out.slice(i, i + length)]));
+                i += length;
+            }
         }
         gifData.push(new Uint8Array([0x00]));
     }
