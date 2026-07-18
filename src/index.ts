0ad771388572a273f4350e08f15748f42a3820c0
speedydelete <speedykorok777@gmail.com>
2026-07-18 12:19:29 -0400

    oops 939

--- src/index.ts
+++ src/index.ts
@@ -395,7 +395,7 @@ async function runCommand(msg: Message): Promise<void> {
         } catch (error) {
             if (error instanceof BotError || error instanceof lifeweb.LifewebError) {
                 previousMsgs.push([msg.id, await msg.reply({content: 'Error: ' + error.message, allowedMentions: {repliedUser: !noReplyPings.includes(msg.author.id), parse: []}})]);
-            } else if (error instanceof Error && error.message === 'Worker exited with code 1!') {
+            } else if (error instanceof Error && (error.message === 'Worker exited with code 1!' || error.message === `ENOENT: no such file or directory, stat '/home/caterer/caterer-ii/sim.gif'` || error.message === `ENOENT: no such file or directory, stat '/home/caterer/caterer-ii/sim_base.gif'`)) {
                 previousMsgs.push([msg.id, await msg.reply({content: `Error: ${error.message} (try running the command again!)`, allowedMentions: {repliedUser: !noReplyPings.includes(msg.author.id), parse: []}})]);
             } else if (error instanceof DiscordAPIError && error.message.match(/Must be (2|4)000 or fewer in length/)) {
                 previousMsgs.push([msg.id, await msg.reply({content: 'Error: Message too long!', allowedMentions: {repliedUser: !noReplyPings.includes(msg.author.id), parse: []}})]);
