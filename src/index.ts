156153850e4f8975ad1578199f9b36ef9dbc712a
speedydelete <speedykorok777@gmail.com>
2026-07-28 12:50:36 -0400

    oops 976

--- src/index.ts
+++ src/index.ts
@@ -428,7 +428,7 @@ async function runCommand(msg: Message): Promise<void> {
                 }
             }
         } catch (error) {
-            if (error instanceof BotError || error instanceof lifeweb.LifewebError) {
+            if (error instanceof BotError || error instanceof lifeweb.LifewebError || error instanceof SyntaxError) {
                 previousMsgs.push([msg.id, await msg.reply({content: `${error.name}: ` + error.message, allowedMentions: {repliedUser: !noReplyPings.includes(msg.author.id), parse: []}})]);
             } else if (error instanceof Error && (error.message === 'Worker exited with code 1!' || error.message === `ENOENT: no such file or directory, stat '/home/caterer/caterer-ii/sim.gif'` || error.message === `ENOENT: no such file or directory, stat '/home/caterer/caterer-ii/sim_base.gif'`)) {
                 previousMsgs.push([msg.id, await msg.reply({content: `${error.name}: ${error.message} (try running the command again!)`, allowedMentions: {repliedUser: !noReplyPings.includes(msg.author.id), parse: []}})]);
