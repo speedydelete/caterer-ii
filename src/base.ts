2399c6baa9f3517da6a40317ac4298cc1c58dec7
speedydelete <speedykorok777@gmail.com>
2026-08-19 11:55:00 -0400

    huh

--- src/base.ts
+++ src/base.ts
@@ -905,6 +905,7 @@ export async function internalRunTextCommand(msg: Message, rawArgs: string): Pro
     let argv = parseArgv(rawArgs);
     let cmd = argv[0][0].toLowerCase().replaceAll('_', '');
     let cmdData = COMMANDS[cmd];
+    console.log(cmd, cmdData);
     console.log(cmdData && cmdData.type === 'basic' && cmdData.noPipe);
     if (argv.some(x => x[0] === '|') && !(cmdData && cmdData.type === 'basic' && cmdData.noPipe)) {
         return await runPipe(msg, rawArgs);
