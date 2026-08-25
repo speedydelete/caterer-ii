d297cb101a1fcc056d2fedcd56157c6a3d33d433
speedydelete <speedykorok777@gmail.com>
2026-08-25 16:50:02 -0400

    ok... (DO NOT !!UPDATE)

---
+++ src/commands/wordle.c
@@ -0,0 +1,315 @@
+
+#include <stdbool.h>
+#include <inttypes.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+
+#define WORD_LENGTH 5
+
+// 5 fields, each field is 2 bits long
+typedef uint32_t Pattern;
+#define GRAY 0
+#define YELLOW 1
+#define GREEN 2
+
+typedef struct Possible {
+    uint32_t count;
+    // array of bools, the length is equal to solutions.len, NOT count
+    bool* data;
+} Possible;
+
+const char* RARE_LETTERS = "KWVZXQJ";
+
+
+static inline void* safe_calloc(size_t size) {
+    void* out = calloc(1, size);
+    if (out == NULL) {
+        perror("Error in calloc");
+        exit(1);
+    }
+    // makes clangd shut up
+    return NULL;
+}
+
+
+typedef struct WordList {
+    size_t len;
+    char (*ptr)[WORD_LENGTH + 1];
+} WordList;
+
+static inline void load_word_list(WordList* out, char* path) {
+    FILE* file = fopen(path, "r");
+    if (file == NULL) {
+        char msg[256];
+        snprintf(msg, sizeof(msg), "Error opening %s", path);
+        perror(msg);
+        exit(1);
+    }
+    size_t len = 0;
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n') {
+            len++;
+        }
+    }
+    rewind(file);
+    out->len = len;
+    out->ptr = safe_calloc(len * (WORD_LENGTH + 1) * sizeof(char));
+    size_t line = 0;
+    size_t i = 0;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n') {
+            line++;
+            i = 0;
+        } else {
+            out->ptr[line][i] = c;
+            i++;
+        }
+    }
+}
+
+static inline void free_word_list(WordList* list) {
+    free(list->ptr);
+}
+
+WordList all_guesses;
+WordList all_solutions;
+
+
+static inline Pattern get_pattern(char* guess, char* target) {
+    static uint8_t counts['z' + 1];
+    // clear counts array
+    memset(&counts['a'], 0, 'z' - 'a' + 1);
+    Pattern pattern = 0;
+    // first resolve greens and add to counts
+    for (int i = 0; i < WORD_LENGTH; i++) {
+        if (guess[i] == target[i]) {
+            pattern |= GREEN * (1 << (i * 2));
+        } else {
+            counts[(int)target[i]]++;
+        }
+    }
+    // next go through counts to add yellows
+    for (int i = 0; i < WORD_LENGTH; i++) {
+        char c = guess[i];
+        if (c != target[i]) {
+            uint8_t value = counts[(int)c];
+            if (value > 0) {
+                pattern |= YELLOW * (1 << (i * 2));
+            }
+        }
+    }
+    return pattern;
+}
+
+static inline void update_possible(Possible* possible, char* guess, char* answer) {
+    Pattern target = get_pattern(guess, answer);
+    for (uint32_t i = 0; i < all_solutions.len; i++) {
+        if (!possible->data[i]) {
+            continue;
+        }
+        if (target == get_pattern(guess, all_solutions.ptr[i])) {
+            possible->data[i] = false;
+            possible->count--;
+        }
+    }
+}
+
+static inline double score_guess(Possible* possible, char* guess) {
+    uint32_t out = 0;
+    for (uint32_t i = 0; i < all_solutions.len; i++) {
+        if (!possible->data[i]) {
+            continue;
+        }
+        Pattern target = get_pattern(guess, all_solutions.ptr[i]);
+        for (uint32_t i = 0; i < all_solutions.len; i++) {
+            if (!possible->data[i]) {
+                continue;
+            }
+            if (target == get_pattern(guess, all_solutions.ptr[i])) {
+                out++;
+            }
+        }
+    }
+    return (double)out / (double)(possible->count);
+}
+
+typedef struct WordAndScore {
+    char* word;
+    double score;
+} WordAndScore;
+
+static inline int word_and_score_sorter(const void* x, const void* y) {
+    return (int)(((WordAndScore*)x)->score - ((WordAndScore*)y)->score);
+}
+
+static inline void rank_guesses(WordAndScore* out, Possible* possible) {
+    for (uint32_t i = 0; i < all_guesses.len; i++) {
+        char* word = all_guesses.ptr[i];
+        out[i].word = word;
+        out[i].score = score_guess(possible, word);
+    }
+    qsort(out, all_guesses.len, sizeof(WordAndScore), word_and_score_sorter);
+}
+
+
+static inline int double_sorter(const void* x, const void* y) {
+    return (int)((*(double*)y) - (*(double*)x));
+}
+
+static inline void to_uppercase(char* out, char* in) {
+    for (int i = 0; i < WORD_LENGTH; i++) {
+        out[i] = toupper(in[i]);
+    }
+}
+
+static inline void rate_game(char** guesses, int guess_count, char* answer) {
+    Possible possible;
+    possible.count = all_solutions.len;
+    possible.data = safe_calloc(all_solutions.len * sizeof(bool));
+    memset(possible.data, true, all_solutions.len);
+    Possible next_possible;
+    next_possible.count = all_solutions.len;
+    next_possible.data = safe_calloc(all_solutions.len * sizeof(bool));
+    memset(next_possible.data, true, all_solutions.len);
+    double total_skill = 0;
+    double total_luck = 0;
+    size_t previous_guesses_size = (guess_count * WORD_LENGTH + 1) * sizeof(char);
+    char* previous_guesses = safe_calloc(previous_guesses_size);
+    memset(previous_guesses, '\0', previous_guesses_size);
+    WordAndScore* data = safe_calloc(all_guesses.len * sizeof(WordAndScore));
+    double* distr = safe_calloc(all_solutions.len * sizeof(WordAndScore));
+    for (int i = 0; i < guess_count; i++) {
+        char* guess = guesses[i];
+        memcpy(&next_possible, &possible, all_solutions.len * sizeof(bool));
+        update_possible(&next_possible, guess, answer);
+        rank_guesses(data, &possible);
+        uint32_t skill_index;
+        for (skill_index = 0; skill_index < all_guesses.len; skill_index++) {
+            if (strcmp(guess, data[skill_index].word) == 0) {
+                break;
+            }
+        }
+        if (skill_index == all_guesses.len) {
+            fprintf(stderr, "Error while finding skill");
+            exit(1);
+        }
+        double guess_score = data[skill_index].score;
+        double best_guess_score = data[all_guesses.len - 1].score;
+        double skill;
+        if (guess_score == best_guess_score) {
+            skill = 100.0;
+        } else {
+            skill = skill_index * 100.0 / all_guesses.len;
+        }
+        // calculate luck: find the distribution of next guesses
+        // and rank it by its position in there
+        uint32_t loc = 0;
+        for (uint32_t i = 0; i < possible.count; i++) {
+            if (!possible.data[i]) {
+                continue;
+            }
+            Pattern target = get_pattern(guess, all_solutions.ptr[i]);
+            double value = 0.0;
+            for (uint32_t i = 0; i < all_solutions.len; i++) {
+                if (!possible.data[i]) {
+                    continue;
+                }
+                if (target == get_pattern(guess, all_solutions.ptr[i])) {
+                    value++;
+                }
+            }
+            distr[loc] = value;
+            loc++;
+        }
+        size_t distr_size = loc;
+        qsort(distr, distr_size, sizeof(double), double_sorter);
+        uint32_t luck_index;
+        for (luck_index = 0; luck_index < all_guesses.len; luck_index++) {
+            if (next_possible.count == distr[luck_index]) {
+                break;
+            }
+        }
+        if (luck_index == all_guesses.len) {
+            fprintf(stderr, "Error while finding luck");
+            exit(1);
+        }
+        double luck;
+        if (distr[luck_index] == distr[distr_size - 1]) {
+            luck = 100.0;
+        } else {
+            luck = luck_index * 100.0 / all_guesses.len;
+        }
+        if (i > 0) {
+            total_skill += skill;
+        }
+        total_luck += luck;
+        char* emoji;
+        if (strncmp(guess, answer, WORD_LENGTH) == 0) {
+            emoji = ":winner:";
+        } else if (possible.count == 1) {
+            emoji = ":miss:";
+        } else if (skill == 100) {
+            emoji = ":best:";
+            if (possible.count > 2) {
+                for (int i = 0; RARE_LETTERS[i] != '\0'; i++) {
+                    char letter = RARE_LETTERS[i];
+                    if (strchr(guess, letter) != NULL && strchr(previous_guesses, letter) == NULL) {
+                        emoji = ":brilliant:";
+                        break;
+                    }
+                }
+            }
+        } else if (skill > 90) {
+            emoji = ":excellent:";
+            if (possible.count > 2) {
+                for (int i = 0; RARE_LETTERS[i] != '\0'; i++) {
+                    char letter = RARE_LETTERS[i];
+                    if (strchr(guess, letter) != NULL && strchr(previous_guesses, letter) == NULL) {
+                        emoji = ":great:";
+                        break;
+                    }
+                }
+            }
+        } else if (skill > 70) {
+            emoji = ":good:";
+        } else if (skill > 50) {
+            emoji = ":inaccuracy:";
+        } else if (skill > 30) {
+            emoji = ":mistake:";
+        } else {
+            emoji = ":blunder:";
+        }
+        char u_guess[WORD_LENGTH + 1];
+        to_uppercase(u_guess, guess);
+        char u_best1[WORD_LENGTH + 1];
+        to_uppercase(u_best1, data[all_guesses.len - 1].word);
+        char u_best2[WORD_LENGTH + 1];
+        to_uppercase(u_best2, data[all_guesses.len - 2].word);
+        char u_best3[WORD_LENGTH + 1];
+        to_uppercase(u_best3, data[all_guesses.len - 3].word);
+        char u_worst[WORD_LENGTH + 1];
+        to_uppercase(u_worst, data[0].word);
+        printf("%s `%s` - %i skill, %i luck, score: %.3f (best: %.3f), ranking: `%s/%s/%s/.../%s`\n", emoji, u_guess, (int)trunc(skill), (int)trunc(luck), guess_score, best_guess_score, u_best1, u_best2, u_best3, u_worst);
+        strncat(previous_guesses, guess, WORD_LENGTH);
+    }
+    printf("Overall: %i skill, %i luck\n", (int)trunc(total_skill / (guess_count - 1)), (int)trunc(total_luck / guess_count));
+    free(data);
+}
+
+
+int main(int argc, char** argv) {
+    if (argc < 4) {
+        fprintf(stderr, "Expected at least 4 arguments");
+    }
+    load_word_list(&all_guesses, argv[1]);
+    load_word_list(&all_solutions, argv[2]);
+    rate_game((char**)argv[4], argc - 4, argv[3]);
+    free_word_list(&all_guesses);
+    free_word_list(&all_solutions);
+    return 0;
+}
--- src/commands/wordle.ts
+++ src/commands/wordle.ts
@@ -1,246 +1,27 @@
 
-import {BotError, readFile} from '../real_base.js';
-// import {BotError, readFile, requiredVariadicArg, addCommand} from '../base.js';
+import {execSync} from 'node:child_process';
+import {BotError, resolvePath, requiredVariadicArg, addCommand} from '../base.js';
 
-
-interface GameInfo {
-    chars: string;
-    length: number;
-    guesses: Set<string>;
-    answers: Set<string>;
-    startRatings: [string, number][];
-    bookStarts: Set<string>;
-    rareLetters: Set<string>;
-}
-
-async function loadWordList(path: string): Promise<Set<string>> {
-    let text = (await readFile(path)).toString();
-    let out = new Set<string>();
-    for (let word of text.split('\n')) {
-        if (word.length > 0) {
-            out.add(word);
-        }
-    }
-    return new Set(Array.from(out).sort());
-}
-
-const DEFAULT: GameInfo = {
-    chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    length: 5,
-    guesses: await loadWordList('data/wordle_guesses.txt'),
-    answers: await loadWordList('data/wordle_answers.txt'),
-    startRatings: Array.from(await loadWordList('data/wordle_guesses.txt')).map(x => [x, 67]),
-    bookStarts: new Set(['SALET', 'QAJAQ', 'CRANE', atob('UEVOSVM='), 'SLATE', 'ADIEU', 'AUDIO', 'OUIJA', 'STARE', 'RAISE', 'ARISE']),
-    rareLetters: new Set('KWVZXQJ'),
-};
-
-
-// algorithm 1:
-
-// 5 fields, each field is 2 bits long
-const GRAY = 0;
-const YELLOW = 1;
-const GREEN = 2;
-
-// function formatPattern(info: GameInfo, pattern: number): string {
-//     return Array.from(pattern.toString(4).padStart(5, '0')).reverse().join('');
-// }
-
-let getPatternCounts = new Uint8Array(256);
-function getPattern(info: GameInfo, guess: string, target: string): number {
-    let counts = getPatternCounts;
-    counts.fill(0);
-    // for (let char of info.chars) {
-    //     counts[char.charCodeAt(0)] = 0;
-    // }
-    for (let i = 0; i < info.length; i++) {
-        counts[i] = 0;
-    }
-    let pattern = 0;
-    for (let i = 0; i < info.length; i++) {
-        if (guess[i] === target[i]) {
-            pattern |= GREEN * (1 << (i * 2));
-            // console.log('green', i, formatPattern(info, pattern));
+addCommand(
+    'ratewordle', 'other', [],
+    'Rates a game of Wordle like chess.com would',
+    [
+        requiredVariadicArg('words', 'string', 'The words to submit, last one should be the answer (if you didn\'t get it, put it at the end anyway'),
+    ],
+    async args => {
+        let words = args.words.map(word => word.toUpperCase());
+        let guesses: string[] = [];
+        let answer: string;
+        if (words.length > 7) {
+            throw new BotError(`More than 7 words provided`);
+        } else if (words.length === 7) {
+            guesses = words.slice(0, 6);
+            answer = words[6];
         } else {
-            counts[target[i].charCodeAt(0)]++;
+            guesses = words;
+            answer = words[words.length - 1];
         }
-    }
-    for (let i = 0; i < info.length; i++) {
-        let char = guess[i];
-        if (char !== target[i]) {
-            let value = counts[char.charCodeAt(0)];
-            if (value > 0) {
-                pattern |= YELLOW * (1 << (i * 2));
-                counts[char.charCodeAt(0)] = value - 1;
-                // console.log('yellow', i, formatPattern(info, pattern));
-            }
-        }
-    }
-    return pattern;
-}
-
-function updateKnown(info: GameInfo, guess: string, answer: string, data: Set<string>): Set<string> {
-    let out = new Set<string>();
-    let target = getPattern(info, guess, answer);
-    for (let word of data) {
-        if (target === getPattern(info, guess, word)) {
-            out.add(word);
-        }
-    }
-    return out;
-}
-
-// information theory based (not optimal!)
-
-function getGuessScore(info: GameInfo, possible: Set<string>, guess: string): number {
-    let out = 0;
-    for (let answer of possible) {
-        let target = getPattern(info, guess, answer);
-        for (let word of possible) {
-            if (target === getPattern(info, guess, word)) {
-                out++;
-            }
-        }
-    }
-    return out / possible.size;
-}
-
-function getAllGuesses(info: GameInfo, possible: Set<string>): [string, number][] {
-    let out: [string, number][] = [];
-    let prevStart = '';
-    for (let guess of info.guesses) {
-        if (guess[0] !== prevStart) {
-            prevStart = guess[0];
-            console.log(guess);
-        }
-        out.push([guess, getGuessScore(info, possible, guess)]);
-    }
-    return out.sort((x, y) => x[1] - y[1]);
-}
-
-
-// rank start words
-
-// let info = DEFAULT;
-// let out: [string, number][] = [];
-// console.log('Running');
-// let guesses = Array.from(info.guesses);
-// let start = performance.now();
-// for (let i = 0; i < guesses.length; i++) {
-//     if (i % 10 === 0 && i > 0) {
-//         out = out.sort((x, y) => x[1] - y[1]);
-//         await writeFile('starts.json', JSON.stringify(out));
-//         console.log(`Checked ${i} guesses: next: ${guesses[i]}, best: ${out[0][0]} (${out[0][1].toFixed(3)}), worst: ${out[out.length - 1][0]} (${out[out.length - 1][1].toFixed(3)}), ${((i / ((performance.now() - start) / 1000))).toFixed(3)} checks/second`);
-//     }
-//     let guess = guesses[i];
-//     out.push([guess, getGuessScore(info, DEFAULT.answers, guess)]);
-// }
-// out = out.sort((x, y) => x[1] - y[1]);
-// await writeFile('starts.json', JSON.stringify(out));
-// console.log('Results:');
-// console.log(out.map(x => `${x[0]} (${x[1].toFixed(3)})`).join('\n'));
-
-
-function rateGame(info: GameInfo, guesses: string[], answer: string): string {
-    let totalSkill = 0;
-    let totalLuck = 0;
-    let totalCountingGuesses = 0;
-    let perWord: string[] = [];
-    let possible = info.answers;
-    for (let i = 0; i < guesses.length; i++) {
-        let guess = guesses[i];
-        let nextPossible = updateKnown(info, guess, answer, possible);
-        let data = i === 0 ? info.startRatings.slice() : getAllGuesses(info, possible);
-        data = data.sort((x, y) => y[1] - x[1]);
-        let bestScore = data[data.length - 1][1];
-        let skill = data.findIndex(x => guess === x[0]);
-        if (skill === -1) {
-            throw new BotError(`Failed to calculate skill for guess ${i}`);
-        }
-        let guessScore = data[skill][1];
-        if (bestScore === guessScore) {
-            skill = 100;
-        } else {
-            skill = skill * 100 / data.length;
-        }
-        // calculate luck: find the distribution of next guesses
-        // and rank it by its position in there
-        let distr = data.map(x => x[1]);
-        let luck = distr.findIndex(x => x === nextPossible.size);
-        if (luck === -1) {
-            luck = 67;
-            // throw new BotError(`Failed to calculate luck for guess ${i}`);
-        }
-        if (distr[distr.length - 1] === distr[luck]) {
-            luck = 100;
-        } else {
-            luck = luck * 100 / data.length;
-        }
-        if (i > 0) {
-            totalSkill += skill;
-            totalLuck += luck;
-            totalCountingGuesses++;
-        }
-        let emoji: string;
-        if (guess === answer) {
-            emoji = '<:winner:1541545248935907418>';
-        }
-        if (i === 0 && info.bookStarts.has(guess)) {
-            emoji = '<:book:1541544540962295818>';
-        } else if (skill === 100) {
-            emoji = '<:best:1541544471345504326>';
-            for (let letter of info.rareLetters) {
-                if (!answer.includes(letter) && guess.includes(letter)) {
-                    emoji = '<:brilliant:1541544576249110649>';
-                    break;
-                }
-            }
-        } else if (skill > 90) {
-            emoji = '<:excellent:1541545113413492877>';
-            for (let letter of info.rareLetters) {
-                if (!answer.includes(letter) && guess.includes(letter)) {
-                    emoji = '<:great:1541543749379952690>';
-                    break;
-                }
-            }
-        } else if (skill > 70) {
-            emoji = '<:good:1541545006823641192>';
-        } else if (skill > 50) {
-            emoji = '<:inaccuracy:1541543842233327727>'
-        } else if (skill > 30) {
-            emoji = '<:mistake:1541544273999167488>';
-        } else {
-            emoji = '<:blunder:1541544504182571108>';
-        }
-        perWord.push(`${emoji} \`${guess}\` - ${skill} skill, ${luck} luck, score: ${guessScore}, best score: ${bestScore}, ranking: ${data.slice(0, 3).map(x => x[0]).join('/')}/.../${data[data.length - 1][0]}`);
-        possible = nextPossible;
-        console.log(possible.size);
-    }
-    return `Overall: ${Math.round(totalSkill / totalCountingGuesses)} skill, ${Math.round(totalLuck / totalCountingGuesses)}\n${perWord.join('\n')} luck`;
-}
-
-
-console.log(rateGame(DEFAULT, ['SALET', 'CRONY', 'RUNNY'], 'RUNNY'));
-
-// addCommand(
-//     'ratewordle', 'other', [],
-//     'Rates a game of Wordle like chess.com would',
-//     [
-//         requiredVariadicArg('words', 'string', 'The words to submit, last one should be the answer (if you didn\'t get it, put it at the end anyway'),
-//     ],
-//     async args => {
-//         let words = args.words.map(word => word.toUpperCase());
-//         let guesses: string[] = [];
-//         let answer: string;
-//         if (words.length > 7) {
-//             throw new BotError(`More than 7 words provided`);
-//         } else if (words.length === 7) {
-//             guesses = words.slice(0, 6);
-//             answer = words[6];
-//         } else {
-//             guesses = words;
-//             answer = words[words.length - 1];
-//         }
-//         return {type: 'string', value: rateGame(DEFAULT, guesses, answer)};
-//     },
-// );
+        let out = await execSync(resolvePath('wordle'), resolvePath('data/wordle_'));
+        return {type: 'string', value: rateGame(DEFAULT, guesses, answer)};
+    },
+);
