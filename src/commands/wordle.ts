6a8a984b5e4a1dc4255a35488f2cb598dbd8a08d
speedydelete <speedykorok777@gmail.com>
2026-08-25 21:10:03 -0400

    ok... its time for LOTS of segfaults!

--- src/commands/wordle.ts
+++ src/commands/wordle.ts
@@ -2,6 +2,9 @@
 import {execSync} from 'node:child_process';
 import {BotError, resolvePath, requiredVariadicArg, addCommand} from '../base.js';
 
+
+const LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+
 addCommand(
     'ratewordle', 'other', [],
     'Rates a game of Wordle like chess.com would',
@@ -9,7 +12,12 @@ addCommand(
         requiredVariadicArg('words', 'string', 'The words to submit, last one should be the answer (if you didn\'t get it, put it at the end anyway'),
     ],
     async args => {
-        let words = args.words.map(word => word.toUpperCase());
+        let words = args.words.map(word => word.toLowerCase());
+        for (let word of words) {
+            if (!Array.from(word).every(letter => LETTERS.includes(letter))) {
+                throw new BotError(`Invalid word: '${word}'`);
+            }
+        }
         let guesses: string[] = [];
         let answer: string;
         if (words.length > 7) {
@@ -21,7 +29,7 @@ addCommand(
             guesses = words;
             answer = words[words.length - 1];
         }
-        let out = await execSync(resolvePath('wordle'), resolvePath('data/wordle_'));
-        return {type: 'string', value: rateGame(DEFAULT, guesses, answer)};
+        let out = execSync(`${resolvePath('wordle')} ${resolvePath('data/wordle_guesses.txt')} ${resolvePath('data/wordle_solutions.txt')} ${answer} ${guesses.join(' ')}`).toString();
+        return {type: 'string', value: out};
     },
 );
